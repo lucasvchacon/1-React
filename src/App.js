9a05f6253b319aabb1a2c3821ea97b219356735e
LucasChacon <lucasvieirac@yahoo.com.br>
2021-09-30 22:48:23 -0300

    2.1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+   <form>
+     <input type="text" placeholder="titulo"/>
+        <textarea placeholder="Escreva sua nota;"/>
+        <button>Criar Nota</button>
+   </form>
+  );
+}
+
+export default App;
